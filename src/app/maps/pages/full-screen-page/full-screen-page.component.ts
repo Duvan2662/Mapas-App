3cbea1116d62386f55e3d70f3df29a08b4c96287
Duvan <duvanriano2626@gmail.com>
2024-08-22 15:47:41 -0500

    Se usa la documentacion de mapbox para la visualizacion del mapa atento con la documentacion de inicio

--- src/app/maps/pages/full-screen-page/full-screen-page.component.ts
+++ src/app/maps/pages/full-screen-page/full-screen-page.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
+
+import mapboxgl from 'mapbox-gl'; // or "const mapboxgl = require('mapbox-gl');"
+
+mapboxgl.accessToken = 'pk.eyJ1IjoiZHV2YW4xNyIsImEiOiJjbTA1cGJoa20wamF3Mm1vZXpuZzhvc2FzIn0.uKGdT4Lf5qV7uf-q10PBfA';
+
+
 
 @Component({
   selector: 'app-full-screen-page',
   templateUrl: './full-screen-page.component.html',
   styleUrl: './full-screen-page.component.css'
 })
-export class FullScreenPageComponent {
+export class FullScreenPageComponent implements AfterViewInit{
+
+  ngAfterViewInit(): void {
+    const map = new mapboxgl.Map({
+      container: 'map', // container ID
+      style: 'mapbox://styles/mapbox/streets-v12', // style URL
+      center: [-74.5, 40], // starting position [lng, lat]
+      zoom: 9, // starting zoom
+    });
+  }
 
 }
