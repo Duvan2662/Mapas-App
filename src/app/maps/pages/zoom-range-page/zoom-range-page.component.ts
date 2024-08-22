ba797f5288acc1ec9fa93fd18b0be61321c194f5
Duvan <duvanriano2626@gmail.com>
2024-08-22 16:53:24 -0500

    Controlar el nivel del zoom mediante los botones la barra y la rueda del mouse

--- src/app/maps/pages/zoom-range-page/zoom-range-page.component.ts
+++ src/app/maps/pages/zoom-range-page/zoom-range-page.component.ts
@@ -9,8 +9,13 @@ import {Map} from 'mapbox-gl'; // or "const mapboxgl = require('mapbox-gl');"
 
 export class ZoomRangePageComponent implements AfterViewInit {
 
-  @ViewChild('map')
-  public divMap?:ElementRef
+
+  @ViewChild('map') public divMap?:ElementRef
+
+  public zoom : number = 10;
+  public map? : Map ;
+
+
 
 
   ngAfterViewInit(): void {
@@ -18,12 +23,48 @@ export class ZoomRangePageComponent implements AfterViewInit {
     throw "El elemento HTML no fue encontrado";
 
    }
-    const map = new Map({
+    this.map = new Map({
       container: this.divMap.nativeElement, // container HTML element
       style: 'mapbox://styles/mapbox/streets-v12', // style URL
       center: [-74.5, 40], // starting position [lng, lat]
-      zoom: 9, // starting zoom
+      zoom: this.zoom, // starting zoom
     });
+
+    this.mapListeners();
+  }
+
+
+
+  public mapListeners = ():void => {
+    if (!this.map) {
+      throw "Mapa no inicializado";
+    }
+    this.map.on('zoom', (evento) => {
+      this.zoom = this.map!.getZoom();
+    })
+
+    this.map.on('zoomend', (evento) => {
+      if (this.map!.getZoom()<18) {
+        return
+      }
+      this.map!.zoomTo(18);
+
+    })
+
+
+
+  }
+
+  public zoomIn = ():void => {
+    this.map?.zoomIn();
+  }
+  public zoomOut = ():void => {
+    this.map?.zoomOut();
+  }
+
+  public zoomChanged = (value: string)  => {
+    this.zoom = Number(value);
+    this.map?.zoomTo(this.zoom)
   }
 
 }
