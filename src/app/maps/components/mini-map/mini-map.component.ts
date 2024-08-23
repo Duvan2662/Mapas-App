bd8cffcc4ba2f0501b4e453f9fa12283cbe99dc1
Duvan <duvanriano2626@gmail.com>
2024-08-22 19:40:21 -0500

    Implementacion del componente mini-map para la creacion del mapa con su marcador

--- src/app/maps/components/mini-map/mini-map.component.ts
+++ src/app/maps/components/mini-map/mini-map.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, Input, ViewChild } from '@angular/core';
+import {Map, Marker} from 'mapbox-gl'; // or "const mapboxgl = require('mapbox-gl');"
 
 @Component({
   selector: 'maps-mini-map',
   templateUrl: './mini-map.component.html',
   styleUrl: './mini-map.component.css'
 })
-export class MiniMapComponent {
+export class MiniMapComponent implements AfterViewInit {
+
+  @Input() lngLat?:[number,number]
+  @ViewChild('map') public divMap?:ElementRef
+
+  public map? : Map ;
+
+  ngAfterViewInit(): void {
+
+    if(!this.divMap?.nativeElement){
+      throw 'Map Div not Found';
+    }
+    if (!this.lngLat) {
+      throw 'Latitud y longitud requeridas';
+    }
+
+    this.map = new Map({
+      container: this.divMap.nativeElement, // container HTML element
+      style: 'mapbox://styles/mapbox/streets-v12', // style URL
+      center: this.lngLat, // starting position [lng, lat]
+      zoom: 15, // starting zoom
+      interactive:false
+    });
+    new Marker()
+    .setLngLat(this.lngLat)
+    .addTo(this.map)
+
+  }
 
 }
